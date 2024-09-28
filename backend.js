8c0977a2bbdd22c3521df7650aabe2fb15f91ee1
aryaidnani <idnaniarya@gmail.com>
2024-09-28 17:14:27 +0530

    Fullstack test

---
+++ backend.js
@@ -0,0 +1,40 @@
+const { userDataModel } = require("./db");
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://admin:MS10DwmRLt0BWiru@cluster0.nstku.mongodb.net/fullstack-demo"
+);
+
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/", (req, res) => {
+  let userName = req.query.userName;
+  let userId;
+
+  userDataModel.create({
+    userId: userId,
+    userName: userName,
+  });
+
+  res.json({
+    message: "User Created Successfully!",
+    userId: userId,
+    userName: userName,
+  });
+});
+
+app.get("/", async (req, res) => {
+  const user = req.query.userName;
+  const foundUser = await userDataModel.find({ userName: user });
+
+  res.json({
+    foundUser,
+  });
+});
+
+app.listen(443);
---
+++ index.js
@@ -0,0 +1,18 @@
+async function submit() {
+  let user = document.querySelector(".usernameInput").value;
+  console.log(user);
+  await axios.post(`http://localhost:443/?userName=${user}`).then((res) => {
+    console.log(res.data);
+  });
+}
+
+async function find() {
+  let user = document.querySelector(".userFindInput").value;
+  console.log(user);
+  await axios.get(`http://localhost:443/?userName=${user}`).then((res) => {
+    document.querySelector(
+      "h1"
+    ).textContent = `Welcome ${res.data.foundUser[0].userName}`;
+    console.log(res.data.foundUser[0].userName);
+  });
+}
