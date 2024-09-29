ff87c8cce6763eb791b713bedfd71ca12caa7fa4
aryaidnani <idnaniarya@gmail.com>
2024-09-29 13:00:05 +0530

    Changed from axios to fetch

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 async function submit() {
-  body = {
-    userName: document.querySelector(".usernameInput").value,
-  };
+  // body = {
+  //   userName: document.querySelector(".usernameInput").value,
+  // };
 
-  console.log(body.userName);
-  await axios.post(`https://fullstack-tester.vercel.app/`, body).then((res) => {
-    console.log(res);
+  // console.log(body.userName);
+  // await axios.post(`https://fullstack-tester.vercel.app/`, body).then((res) => {
+  //   console.log(res);
+  // });
+  let usernameVal = document.querySelector(".usernameInput").value;
+  console.log(usernameVal);
+
+  const res = await fetch("https://fullstack-tester.vercel.app/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userName: usernameVal }),
   });
+
+  console.log(await res.json());
 }
 
 async function findUser() {
