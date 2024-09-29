be4dedb203d4e1c451044d77cdeb56ca95c17869
aryaidnani <idnaniarya@gmail.com>
2024-09-30 05:06:54 +0530

    Back to type script

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,10 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/arya", (req, res) => res.send("Hello Arya!"));
+
+app.get("/jon", (req, res) => res.send("Hello Jon Snow!"));
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
