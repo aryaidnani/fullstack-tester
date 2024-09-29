f8b32be2524b31e70e25e9aee21e9d528045ad80
aryaidnani <idnaniarya@gmail.com>
2024-09-30 04:43:39 +0530

    -

---
+++ api/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/arya", (req, res) => {
+  res.send("Hello Arya 1");
+});
+
+app.get("/arya2", (req, res) => {
+  res.send("Hello Arya 2");
+});
+
+app.listen(PORT);
+
+module.exports = app;
