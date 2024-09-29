6e31a3ec52858333335632592453435c6e6aff7e
aryaidnani <idnaniarya@gmail.com>
2024-09-29 13:32:32 +0530

    isudhf

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function submit() {
   let usernameVal = document.querySelector(".usernameInput").value;
   console.log(usernameVal);
 
-  const res = await fetch("https://fullstack-tester.vercel.app/api/backend", {
+  const res = await fetch("https://fullstack-tester.vercel.app/api/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
