bbb9b06138d0234d34481952034a0a610020485d
aryaidnani <idnaniarya@gmail.com>
2024-09-30 04:45:50 +0530

    -

--- api/app.js
+++ api/app.js
@@ -1,24 +1,8 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-const PORT = 3000;
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/arya", (req, res) => {
-  res.send("Hello Arya 1");
-});
-
-app.get("/arya2", (req, res) => {
-  res.send("Hello Arya 2");
-});
-
-app.listen(PORT);
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
