d0694a981c3e94cf3130fe298cc7666498a66f56
aryaidnani <idnaniarya@gmail.com>
2024-10-04 23:30:17 +0530

    back to old working code

--- front.js
+++ front.js
@@ -1,6 +1,6 @@
 async function submit() {
   const username = document.querySelector(".userInput").value;
-  const response = await fetch("https://fullstack-tester.vercel.app/posts", {
+  await fetch("https://fullstack-tester.vercel.app/posts", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
